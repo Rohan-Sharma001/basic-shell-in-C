987b8cbd9a8804eed026364639d664a1b33084d2
Rohan Sharma <rohansharma6610548@gmail.com>
2025-05-20 01:14:10 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]) {
       return 0;
     }
     else if (!strcmp(input, echo_str)) {
-      printf("%s", s[5]);
+      printf("%s", input[5]);
     }
     else {
       printf("%s: command not found\n", input);
