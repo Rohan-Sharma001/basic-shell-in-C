93c5287bb0b92091bf005fa43f88cd9b81f456e7
Rohan Sharma <rohansharma6610548@gmail.com>
2025-05-20 11:34:32 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,31 +2,74 @@
 #include <stdlib.h>
 #include <string.h>
 
+int default_return = -1;
+char *commands[] = {"exit", "echo", "type"};
+int invalid_command(char *inputt);
+int echo(char *inputt);
+int exitt(char *inputt);
+int typef(char *inputt);
+int (*func[]) (char *) = {exitt, echo, typef};
+
+
+
 int main(int argc, char *argv[]) {
-  // Flush after every printf
   setbuf(stdout, NULL);
 
-  // Uncomment this block to pass the first stage
- 
-  // Wait for user input
   char input[100];
   while (1) { 
     printf("$ ");
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = '\0';
-    
-    //exit
-    char *exit_str = "exit 0";
-    char *echo_str = "echo";
-    if (!strcmp(input, exit_str)) {
-      return 0;
+
+    int executed = 0;
+    int ret;
+    for (int i = 0; i < sizeof(func)/sizeof(func[0]); i++) {
+      if (!strncmp(input, commands[i], strlen(commands[i]))) {ret = func[i](input); executed = 1; break;}
     }
-    else if (!strncmp(input, echo_str, 4)) {
-      printf("%s\n", input+5);
+    if (!executed) {
+      invalid_command(input);
     }
-    else {
-      printf("%s: command not found\n", input);
+    else if (ret != default_return) {
+      return ret;
     }
   }
   return 0;
 }
+
+int invalid_command(char *inputt) {
+  printf("%s: command not found\n", inputt);
+  return default_return;
+}
+
+int typef(char *inputt) {
+  if (strlen(inputt) > 4 && inputt[4] != ' ') {
+    invalid_command(inputt);
+  }
+  else {
+    int found = 0;
+    for (int i = 0; i < sizeof(func)/sizeof(func[0]); i++) {
+      if (!strcmp(inputt+5, commands[i])) {printf("%s is a shell builtin\n", commands[i]); found = 1; break;}
+    }
+    if (!found) {
+      printf("%s: not found", inputt+5);
+    }
+  }
+  return default_return;
+}
+
+int exitt(char *inputt) {
+  char *exit_str = "exit 0";
+  if (!strcmp(inputt, exit_str)) {
+    return 0;
+  }
+  else {
+    invalid_command(inputt);
+  }
+  return default_return;
+}
+
+int echo(char *inputt) {
+  printf("%s\n", inputt+5);
+  return default_return;
+}
+
